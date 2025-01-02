452a612d6579e7bc4f11163f8b63128e4d892449
laszlo <chernyackoff.vlad@yandex.ru>
2025-01-02 23:02:52 +0300

    init

---
+++ tcl/init_config.tcl
@@ -0,0 +1,12 @@
+set PROJECT_NAME AXI_correlator
+set PROJECT_DIR AXI_correlator
+set INCREMENTAL_BUILD false
+set FPGA_PART xc7v585tffg1157-2
+set TARGET_LANG VHDL
+set SIM_LANG VHDL
+set SRC_DIR src
+set TCL_DIR tcl
+set SIM_DIR tb
+set CNSTR_DIR cnstr
+set SIM_TOP_FILE_NAME AXI_correlator_TB
+set TOP_FILE_NAME AXI_correlator_TOP
